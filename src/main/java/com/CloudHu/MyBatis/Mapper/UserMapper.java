f0aad06cedef9e9767818ee0dcd6bc5d306502cd
cloud-hu2000 <961035277@qq.com>
2022-03-05 14:16:38 +0800

    第二次内容，更新了增删改查和log4j

--- src/main/java/com/CloudHu/MyBatis/Mapper/UserMapper.java
+++ src/main/java/com/CloudHu/MyBatis/Mapper/UserMapper.java
@@ -2,6 +2,16 @@ package com.CloudHu.MyBatis.Mapper;
 
 import com.CloudHu.MyBatis.POJO.User;
 
+import java.util.List;
+
 public interface UserMapper {
     int insertUser();
+
+    void updateUser();
+
+    void deleteUser();
+
+    User getUserById();
+
+    List<User> getAllUser();
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -8,10 +8,11 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class test {
     @Test
-    public void test(){
+    public void testInsert(){
         try {
             //加载核心配置文件
             InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
@@ -25,6 +26,85 @@ public class test {
             UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
             //测试功能
             userMapper.insertUser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void testUpdate(){
+        try {
+            //加载核心配置文件
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            //获取sqlSessionFactoryBuilder
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //使用配置文件创建的流来获取sqlSessionFactory
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+            //获取sqlSession
+            SqlSession sqlSession=sqlSessionFactory.openSession(true);
+            //获取Mapper接口的实例对象
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            //测试功能
+            userMapper.updateUser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void testDelete(){
+        try {
+            //加载核心配置文件
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            //获取sqlSessionFactoryBuilder
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //使用配置文件创建的流来获取sqlSessionFactory
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+            //获取sqlSession
+            SqlSession sqlSession=sqlSessionFactory.openSession(true);
+            //获取Mapper接口的实例对象
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            //测试功能
+            userMapper.deleteUser();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetUserById(){
+        try {
+            //加载核心配置文件
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            //获取sqlSessionFactoryBuilder
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //使用配置文件创建的流来获取sqlSessionFactory
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+            //获取sqlSession
+            SqlSession sqlSession=sqlSessionFactory.openSession(true);
+            //获取Mapper接口的实例对象
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            //测试功能
+            userMapper.getUserById();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetAllUser(){
+        try {
+            //加载核心配置文件
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            //获取sqlSessionFactoryBuilder
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //使用配置文件创建的流来获取sqlSessionFactory
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
+            //获取sqlSession
+            SqlSession sqlSession=sqlSessionFactory.openSession(true);
+            //获取Mapper接口的实例对象
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            //测试功能
+            List <User> list=userMapper.getAllUser();
+            System.out.println(list.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
